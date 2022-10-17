8b0995b5a5cda2cfbf33f814a68d7ad36df2bc86
Sandip77-hub <72593171+Sandip77-hub@users.noreply.github.com>
2022-10-17 22:04:12 +0545

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Footer()
+{
+    return (
+        <footer className="foot">
+            <img className="Gmail" src="./facebook.jpg"></img>        
+            <img className="Gmail" src="./github.jpg"></img> 
+            <img className="Gmail" src="./twitter.jpg"></img> 
+            <img className="Gmail" src="./gmail.jpg"></img> 
+        </footer>
+
+    )
+}
---
+++ Info.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Info()
+{
+    return (
+        <nav className="All">
+            <img className="Ima" src="./sandip.jpg" />
+            <h1 className="pa">Sandip Pariyar</h1>
+            <button type="button" className="Buty"><b>Email</b></button>
+            <button type="button" className="But" ><b>LinkedIn</b></button>
+        </nav>
+
+    )
+
+}
